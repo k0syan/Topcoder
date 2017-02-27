de71f8fd9b1c07ac45e6263dacc0a79edbe6258f
Shahen Kosyan <k0syan@users.noreply.github.com>
2017-02-27 18:45:59 +0400

    Update genetics.cpp

--- Genetics/genetics.cpp
+++ Genetics/genetics.cpp
@@ -3,26 +3,19 @@
 #include <locale>
 using namespace std;
 
-class Genetics() {
-  public string getOffspring(string g1, string g2, string dom) {
-    string returnString = "";
-
-    for(int i = 0; i < string.length(); ++i) {
-      if (g1[i] == g2[i]) {
-        returnString += g1[i];
-      } else {
-        if(dom[i] == "D") {
-          returnString += toupper(g1[i]);
+class Genetics {
+  public:
+    string getOffspring(string g1, string g2, string dom) {
+      string answer = "";
+      for (int i = 0; i < g1.length(); ++i) {
+        if (g1[i] == g2[i]) {
+          answer += g1[i];
+        } else if (dom[i] == 'D') {
+            answer += toupper(g1[i]);
         } else {
-          returnString += tolower(g2[i]); 
+            answer += tolower(g1[i]);
         }
       }
+      return answer;
     }
-    return returnString;
-  }
 };
-
-
-void main() {
-  
-}
